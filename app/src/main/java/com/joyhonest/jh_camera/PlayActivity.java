9abd92ff8a92fa07fa273d96dff6f1b22e0b345f
小米哥 <joyhonest@126.com>
2021-08-06 15:26:49 +0800

    优化SDK,多线程解码

--- app/src/main/java/com/joyhonest/jh_camera/PlayActivity.java
+++ app/src/main/java/com/joyhonest/jh_camera/PlayActivity.java
@@ -1,14 +1,10 @@
 package com.joyhonest.jh_camera;
 
-import android.media.Image;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import com.joyhonest.wifination.wifination;
 
 public class PlayActivity extends AppCompatActivity implements View.OnClickListener {
 
--- app/src/main/java/com/joyhonest/jh_camera/StartActivity.java
+++ app/src/main/java/com/joyhonest/jh_camera/StartActivity.java
@@ -1,9 +1,8 @@
 package com.joyhonest.jh_camera;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
