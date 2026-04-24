daca894b587ada68259b95de546cc3b11979ca4b
aivenlau <aivenlau@126.com>
2026-04-24 15:54:15 +0800

    7.38

--- jh_wificamera/src/main/java/com/joyhonest/wifination/GP4225_Device.java
+++ jh_wificamera/src/main/java/com/joyhonest/wifination/GP4225_Device.java
@@ -207,7 +207,7 @@ public class GP4225_Device {
             LockedCount = ((data[16] & 0xFF) + (data[17] & 0xFF) * 0x100 + (data[18] & 0xFF) * 0x10000 + (data[19] & 0xFF) * 0x1000000);
             PhotoCount = ((data[20] & 0xFF) + (data[21] & 0xFF) * 0x100 + (data[22] & 0xFF) * 0x10000 + (data[23] & 0xFF) * 0x1000000);
 
-            if (n_len >= 0x36) {
+            if (data.length >= 36) {
                 nSDAllSize = ((data[24] & 0xFF) + (data[25] & 0xFF) * 0x100 + (data[26] & 0xFF) * 0x10000 + (data[27] & 0xFF) * 0x1000000L + (data[34] & 0xFF) * 0x100000000L);
                 nSDAvailableSize = ((data[28] & 0xFF) + (data[29] & 0xFF) * 0x100 + (data[30] & 0xFF) * 0x10000 + (data[31] & 0xFF) * 0x1000000L + (data[35] & 0xFF) * 0x100000000L);
             }
